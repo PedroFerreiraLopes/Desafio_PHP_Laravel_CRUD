8d6eda8320eccc3876d20f329177a47756b75501
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2025-02-22 21:13:49 -0300

    feat: register page with form contents reflecting Usuario Model

--- CRUD_Challenge/resources/views/auth/register.blade.php
+++ CRUD_Challenge/resources/views/auth/register.blade.php
@@ -2,50 +2,78 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Nome -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="nome" :value="__('Nome')" />
+            <x-text-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required autofocus autocomplete="nome" />
+            <x-input-error :messages="$errors->get('nome')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+        <!-- CPF -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="cpf" :value="__('CPF')" />
+            <x-text-input id="cpf" class="block mt-1 w-full" type="number" name="cpf" :value="old('cpf')" required autocomplete="cpf" />
+            <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
         </div>
 
-        <!-- Password -->
+        <!-- Data de Nascimento -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="data_nascimento" :value="__('Data de nascimento')" />
+            <x-text-input id="data_nascimento" class="block mt-1 w-full" type="date" name="data_nascimento" :value="old('data_nascimento')" required autocomplete="data_nascimneto" />
+            <x-input-error :messages="$errors->get('data_nascimento')" class="mt-2" />
+        </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
+        <!-- Telefone -->
+        <div class="mt-4">
+            <x-input-label for="telefone" :value="__('Número de Telefone com DDD')" />
+            <x-text-input id="telefone" class="block mt-1 w-full" type="number" name="telefone" :value="old('telefone')" required autocomplete="telefone" />
+            <x-input-error :messages="$errors->get('telefone')" class="mt-2" />
+        </div>
+
+        <!-- Gênero -->
+        <div class="mt-4">
+            <x-input-label for="genero" :value="__('Seu Gênero')" />
+            <select id="genero" class="block mt-1 w-full" type="number" name="genero" :value="old('genero')" required autocomplete="Genero">
+                <option value="">Selecione o gênero</option>
+                @foreach (App\Models\Usuario::generos() as $genero)
+                    <option value="{{ $genero }}" {{ old('genero') == $genero ? 'selected' : '' }}>
+                        {{ $genero }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+        <!-- Senha -->
+        <div class="mt-4">
+            <x-input-label for="senha" :value="__('Senha')" />
+
+            <x-text-input id="senha" class="block mt-1 w-full"
                             type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                            name="senha"
+                            required autocomplete="new-senha" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('senha')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
+        <!-- Confirmação de Senha-->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="confirmacao_senha" :value="__('Confirme sua Senha')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="confirmacao_senha" class="block mt-1 w-full"
                             type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                            name="senha_confirmation"
+                            required autocomplete="new-senha" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-error :messages="$errors->get('confirmacao_senha')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Já possui uma conta? Clique aqui!') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Registrar') }}
             </x-primary-button>
         </div>
     </form>
