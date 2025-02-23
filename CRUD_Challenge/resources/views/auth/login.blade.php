63101da6a9f229938ad30e3f5bbc65be9ee41a24
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2025-02-22 21:10:43 -0300

    feat: Login page using CPF + senha for authentication

--- CRUD_Challenge/resources/views/auth/login.blade.php
+++ CRUD_Challenge/resources/views/auth/login.blade.php
@@ -5,42 +5,42 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
+        <!-- CPF -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-label for="cpf" :value="__('CPF')" />
+            <x-text-input id="cpf" class="block mt-1 w-full" type="number" name="cpf" :value="old('cpf')" required autocomplete="cpf" />
+            <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
+        <!-- Senha -->
+        <div class="mt-4">
+            <x-input-label for="senha" :value="__('Senha')" />
+
+            <x-text-input id="senha" class="block mt-1 w-full"
+                            type="password"
+                            name="senha"
+                            required autocomplete="current-senha" />
+
+            <x-input-error :messages="$errors->get('senha')" class="mt-2" />
+        </div>
+
+        <!-- Lembrar de mim -->
         <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <label for="lembrar" class="inline-flex items-center">
+                <input id="lembrar" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="lembrar">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Lembrar de mim') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+            {{-- @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
-            @endif
+            @endif --}}
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Entrar') }}
             </x-primary-button>
         </div>
     </form>
