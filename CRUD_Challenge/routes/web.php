bf1ce23ed64ba438d98bcc756db6f964db0d3acf
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2025-02-22 21:08:16 -0300

    feat: propper routing for system's actual state

--- CRUD_Challenge/routes/web.php
+++ CRUD_Challenge/routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/perfil', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
+
+Route::get('/dashboard', [UsuarioController::class, 'index'])->middleware('auth')->name('dashboard');
+Route::delete('/usuario/{usuario}', 
+[UsuarioController::class, 'destroy'])->middleware('auth')->name('usuario.destroy');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::post('/validate', ValidacaoUsuarioController::class)->withoutMiddleware([VerifyCsrfToken::class]);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
